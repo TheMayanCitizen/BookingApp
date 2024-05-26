c3d376bd16c96803b9ee26834e7184fb6a7a58c4
Alejandro Aguilar <98495755+TheMayanCitizen@users.noreply.github.com>
2024-05-25 23:36:35 -0700

    homepage route created, hotelcard created and info displayed and styled

---
+++ src/components/HotelPage/HotelCard.tsx
@@ -0,0 +1,43 @@
+import { Hotel } from "../../store/slices/types/hotels.types";
+import "./styles/HotelCard.css";
+interface Props {
+  hotel: Hotel;
+}
+
+const maxRating = 5;
+export const HotelCard = ({ hotel }: Props) => {
+  return (
+    <article className="card">
+      <header className="card__header">
+        <img src={hotel.images[0].url} alt="" className="card__img" />
+      </header>
+      <section className="card__section">
+        <h3 className="card__name">{hotel.name}</h3>
+        <div className="card__rating">
+          <div className="card__stars">
+            {Array.from({ length: maxRating }, (_, index) => (
+              <i
+                key={index}
+                className={
+                  index < Math.floor(+hotel?.rating)
+                    ? "bx bxs-star"
+                    : "bx bx-star"
+                }
+              ></i>
+            ))}
+          </div>
+          {hotel.rating}
+        </div>
+        <div className="card__location">
+          {hotel.city.name}, {hotel.city.country}
+        </div>
+        <div className="card__price">{hotel.price}</div>
+      </section>
+      <footer className="card__footer">
+        <button className="card__btn" /* onClick={navigateHotelId} */>
+          See more...
+        </button>
+      </footer>
+    </article>
+  );
+};
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../store/hooks";
 import { getHotelsThunk } from "../store/slices/hotels.slice";
+import { HotelCard } from "../components/HotelPage/HotelCard";
+import "./styles/HomePage.css";
 
 export const HomePage = () => {
   const hotels = useAppSelector((state) => state.hotels);
@@ -11,7 +13,13 @@ export const HomePage = () => {
     dispatch(getHotelsThunk(url));
   }, []);
 
-  console.log(hotels);
-
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <div className="cards__container">
+        {hotels?.map((hotel) => (
+          <HotelCard key={hotel.id} hotel={hotel} />
+        ))}
+      </div>
+    </div>
+  );
 };
