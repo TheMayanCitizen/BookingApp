278b1f6f004eef8ccf0d4dce074aa4fb9e250db7
Ismael Alvarez <ismaelalvarez514@gmail.com>
2024-05-28 16:25:13 -0600

    Creation menu responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom";
-
 import { HomePage, HotelIdPage } from "./pages";
+import { NavBar } from "./components";
 
 function App() {
   return (
     <div>
+      <NavBar/>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/hotel/:id" element={<HotelIdPage />} />
--- src/components/shared/NavBar.tsx
+++ src/components/shared/NavBar.tsx
@@ -1,25 +1,29 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export const NavBar = () => {
-  return (
-    <header >
-        <h1>
-            Boking <span>App</span> 
-        </h1>
-        
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/reservation">Reservation</Link>
-                </li>
-                <li>
-                    <Link to="/regiter">Register</Link>
-                </li>
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
-            </ul>
-        </nav>
-    </header>
-  )
+    const [menu, setMenu] = useState<boolean>(false);
+    return (
+        <header className="flex justify-between bg-white shadow-lg py-5 px-10 mb-10 items-center relative">
+            <h1 className="drop-shadow-3xl text-4xl font-medium text-first-color">
+                <Link to="/">Boking<span className="text-firs-color">App</span></Link>
+            </h1>
+           
+           <p className="md:hidden" onClick={()=> setMenu(!menu)}>menu</p>
+
+            <nav className={`md:flex md:gap-5 md:z-0 mt-2 md:mt-0  md:top-0 md:bg-none md:shadow-none md:right-0 right-10 md:translate-y-0 md:relative transition-all duration-500 ease-linear -translate-y-[500px] absolute  top-full ${menu ? "translate-y-0 right-10 bg-white shadow-lg mt-2" : "-z-10 -translate-y-[500px]"}`}>
+                <ul className="flex md:flex-row  flex-col  gap-10  mt-2 md:mt-0 ">
+                    <li className="">
+                        <Link to="/reservation">Reservation</Link>
+                    </li>
+                    <li >
+                        <Link to="/register">Register</Link>
+                    </li>
+                    <li >
+                        <Link to="/login">Login</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
